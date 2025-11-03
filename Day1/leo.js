f1aa29d27667bcecebe2f1b2867df53058a0e05c
Madhavan-12 <mwaran934@gmail.com>
2025-11-03 20:51:05 +0800

    First commit

---
+++ Day1/leo.js
@@ -0,0 +1,7 @@
+function story(hero,sister,guard){
+    return `${hero} is the hero of the movie,his sister ${sister} is killed by
+    his Father.${guard} is guard for ${hero} family.${guard} helps ${hero}
+     to take revenge for ${sister} death.Finally they both defeated his father`
+}
+
+module.exports=story;
---
+++ Day1/loki.js
@@ -0,0 +1,6 @@
+const story =require("./leo")
+
+const castname=story("Leo","Elisa das","Napolean");
+console.log(castname)
+
+//this task for import ans export in node
